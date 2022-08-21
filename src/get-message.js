374695288f07233255e967fe8342a4762e531466
alinceDev <ali.ince@smile.fr>
2022-08-21 12:47:47 +0200

    Add ignore draft input

--- src/get-message.js
+++ src/get-message.js
@@ -12,16 +12,20 @@ const parseEmptyPullRequests = (repoObject) => {
   return slackBlockKit.sectionWithText(message);
 };
 
-const parsePullRequests = ({pullRequests}) => {
+const parsePullRequests = ({pullRequests, ignoreDraft}) => {
   const [{head: {repo: repoObject}}] = pullRequests;
   const head = `${getRepoURL(repoObject)} has the following PRs open:`;
-  const parsePR = pr => `- ${slackBlockKit.link(pr.title, pr.html_url)} by ${pr.user.login}\n`;
+  const parsePR = pr => {
+    if (ignoreDraft === 'false' || ignoreDraft === 'true' && pr.draft === false ) {
+      return `- ${slackBlockKit.link(pr.title, pr.html_url)} by ${pr.user.login}\n`;
+    }
+  };
   const body = pullRequests.map(parsePR).join('');
 
   return slackBlockKit.sectionWithText(`${head}\n${body}`.trim());
 };
 
-module.exports = async ({Octokit, owner, repo}) => {
+module.exports = async ({Octokit, owner, repo, ignoreDraft}) => {
   try {
     const {data: pullRequests} = await Octokit.pulls.list({owner, repo, state: 'open'});
 
@@ -30,7 +34,7 @@ module.exports = async ({Octokit, owner, repo}) => {
       return parseEmptyPullRequests(repoObject);
     }
 
-    return parsePullRequests({pullRequests});
+    return parsePullRequests({pullRequests, ignoreDraft});
   } catch (error) {
     error.message = prefixError(error, 'GitHub');
     throw error;
